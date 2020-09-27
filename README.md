7a9cf78aed8820607075fbebb23d99155380e6f7
Galco6 <victor.galvinco@gmail.com>
2020-09-27 16:10:06 +0200

    Added svg animation to readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # termiband
 A cli to view data from your miband in the terminal using [Gadgetbridge](https://gadgetbridge.org/) database
+
+![termiband demo](demo.svg)
--- tb_cli.py
+++ tb_cli.py
@@ -4,7 +4,7 @@ from datetime import datetime, date, timedelta
 def init_cli():
     parser = argparse.ArgumentParser(description='View your MiBand data analytics from Gadgetbridge database in yor terminal')
     parser.add_argument('-d', nargs=1, type=str, help='Select device')
-    parser.add_argument('-info', action="store_true", help='Show device info')
+    parser.add_argument('--info', action="store_true", help='Show device info')
     parser.add_argument('--steps', type=lambda d: datetime.strptime(d, '%Y-%m-%d'), nargs=2,
                         help='Get steps [from_date] [to_date] optionals to acquire data')
     parser.add_argument('--sleep', action="store_true", help='Show sleep data')
